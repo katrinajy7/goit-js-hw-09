fa3281aebc0f0718e24d16d33dce746873238ba8
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-06-27 13:22:18 +0000

    Deploying to gh-pages from @ katrinajy7/goit-js-hw-09@b99378f1a7a0b84d7ab6d0d83e4d1bda66606aa6 🚀

--- 01-color-switcher.55b5427a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("body"),document.querySelector("button[data-start]");const t=document.querySelector("button[data-stop]");t.disabled=!0;
-//# sourceMappingURL=01-color-switcher.55b5427a.js.map
