c38eb8386bc0757b8919dc6b6fdcccff3c4e93e2
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-06-27 19:40:39 -0700

    UPDATED

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+  }
+
+  const body = document.querySelector('body')
+  const startBtn = document.querySelector('button[data-start]');
+  const stopBtn = document.querySelector('button[data-stop]');
+
+  let colorChangeInterval = null
+  stopBtn.disabled = true;
